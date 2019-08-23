676fea5fc581cdd1ea4607f41b8c5dd39e955a03
PCHRP <fluk.fluk.zuza@gmail.com>
2019-08-23 14:06:27 +0700

    login เข้าไปหน้าแรก

--- TestTerminal8/app/src/main/java/testwork/com/testterminal8/activity/LoginActivity.java
+++ TestTerminal8/app/src/main/java/testwork/com/testterminal8/activity/LoginActivity.java
@@ -1,15 +1,69 @@
 package testwork.com.testterminal8.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import testwork.com.testterminal8.R;
+import testwork.com.testterminal8.manager.Contextor;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText EdUsername,EdPassword;
+    CardView Btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitInstances();
+    }
+
+    private void InitInstances() {
+
+        EdUsername = (EditText)findViewById(R.id.ed_username_login);
+        EdPassword = (EditText)findViewById(R.id.ed_passwork_login);
+        Btnlogin = (CardView)findViewById(R.id.btnLogin);
+
+        Btnlogin.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == Btnlogin){
+            checkUser();
+        }
+    }
+
+    private void checkUser() {
+
+        String user,pass;
+        user = EdUsername.getText().toString();
+        pass = EdPassword.getText().toString();
+
+        if(user.equals("fluk")){
+            if(pass.equals("1234")){
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                finish();
+                startActivity(intent);
+            }
+            else {
+                Toast.makeText(Contextor.getInstance().getmContext(),"รหัสผ่านผิด",Toast.LENGTH_LONG).show();
+            }
+        }
+        else{
+            Toast.makeText(Contextor.getInstance().getmContext(),"รหัสผ่านผิด",Toast.LENGTH_LONG).show();
+        }
+
+
+
     }
 }
