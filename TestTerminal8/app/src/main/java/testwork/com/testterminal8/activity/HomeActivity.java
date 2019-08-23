babab842a10475bc7d07b9605d80bafec628dd63
PCHRP <fluk.fluk.zuza@gmail.com>
2019-08-23 13:37:11 +0700

    list ทั้งสองหน้า

---
+++ TestTerminal8/app/src/main/java/testwork/com/testterminal8/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package testwork.com.testterminal8.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import testwork.com.testterminal8.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- TestTerminal8/app/src/main/java/testwork/com/testterminal8/activity/LoginActivity.java
+++ TestTerminal8/app/src/main/java/testwork/com/testterminal8/activity/LoginActivity.java
@@ -1,13 +1,15 @@
-package testwork.com.testterminal8;
+package testwork.com.testterminal8.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import testwork.com.testterminal8.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
